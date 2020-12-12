dd3a9833264020db77fce5fb7c255443ae3902ee
DYC <dy.coster@gmail.com>
2020-12-12 13:41:21 +0100

    Start API and eventListener

--- Project03/weather-journal-app/website/app.js
+++ Project03/weather-journal-app/website/app.js
@@ -1,5 +1,23 @@
 /* Global Variables */
+const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = 'f2e89ddc1b6bbdf69468c5b14dccd167';
+
+// add eventlistener with callback action
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+e.preventDefault();
+const zip = document.getElementById('zip').value;
+const content = document.getElementById('feelings').value;
+}
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+
+// Fetch openweatherMap API
+const getData = async (baseURL, zip, apiKey) => {
+      const response = await fetch(baseURL + zip + apiKey);
+    }
