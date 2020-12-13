d00f0a23683ff7f3d63e4322723e2736954698a3
DYC <dy.coster@gmail.com>
2020-12-13 11:22:10 +0100

    Post Request start

--- Project03/weather-journal-app/website/app.js
+++ Project03/weather-journal-app/website/app.js
@@ -1,19 +1,21 @@
 /* Global Variables */
-const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
-const apiKey = 'f2e89ddc1b6bbdf69468c5b14dccd167';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '1fe34768e8f763c73fb8af6f2bf45812';
+const newZip = document.getElementById('zip').value;
+const newFeeling = document.getElementById('feelings').value;
+
 
 // add eventlistener with callback action
 document.getElementById('generate').addEventListener('click', performAction);
 
 function performAction(e) {
-const newZip = document.getElementById('zip').value;
 getWeather(baseURL, newZip, apiKey)
-
 }
 
 // Fetch openweatherMap API
 const getWeather = async (baseURL, newZip, apiKey) => {
-    const res = await fetch(baseURL + newZip + apiKey);
+
+    const res = await fetch(baseURL+newZip+apiKey);
     try {
 
         const data = await res.json();
@@ -23,6 +25,33 @@ const getWeather = async (baseURL, newZip, apiKey) => {
         console.log("error", error);
     }
   }
+
+
+// Async POST request
+
+const postData = async (url = '', data = {})=> {
+    console.log(data);
+
+    const res = await fetch (url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    }catch(error) {
+        console.log("error", error);
+    }
+}
+
+
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
