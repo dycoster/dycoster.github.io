8722f9686db66ea34dbf075eab2c7a4f44486f3f
DYC <dy.coster@gmail.com>
2020-12-31 11:28:04 +0100

    troubleshooting API

--- Project04/evaluate-news-nlp/src/client/js/formHandler.js
+++ Project04/evaluate-news-nlp/src/client/js/formHandler.js
@@ -7,13 +7,31 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => {
-        return res.json()
-    })
-    .then(function(data) {
-        document.getElementById('results').innerHTML = data.message;
+
+    postData('http://localhost:8081/add', {text: formText})
+
+    .then(function(res) {
+        document.getElementById('results').innerHTML = res.score_tag;
     })
 }
 
+const postData = async (url = '', data = {})=> {
+    const response = await fetch (url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    }catch (error) {
+        console.log("error", error);
+    }
+}
 export { handleSubmit }
+
--- Project04/evaluate-news-nlp/src/server/index.js
+++ Project04/evaluate-news-nlp/src/server/index.js
@@ -1,35 +1,48 @@
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(`Your API key is ${process.env.API_KEY}`);
+
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 var bodyParser = require('body-parser')
-var cors = require('cors')
+const fetch = require('node-fetch')
 
-var json = {
-    'title': 'test json response',
-    'message': 'this is a message',
-    'time': 'now'
-}
 
-var textapi = new meaningCloud ({
-    application_id: process.env.API_ID,
-    application_key: process.env.API_KEY
-});
+// var json = {
+//     'title': 'test json response',
+//     'message': 'this is a message',
+//     'time': 'now'
+// }
+
+// var textapi = new meaningCloud ({
+//     application_id: process.env.API_ID,
+//     application_key: process.env.API_KEY
+// });
 
 const app = express()
+
+const cors = require('cors')
+
 app.use(cors())
 // to use json
 app.use(bodyParser.json())
 // to use url encoded values
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: false
 }))
-
+app.use(bodyParser.json())
 app.use(express.static('dist'))
 
-console.log(JSON.stringify(mockAPIResponse))
+// console.log(JSON.stringify(mockAPIResponse))
+
+// API
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?'
+const apiKey = process.env.API_KEY
+console.log(`Your API key is ${process.env.API_KEY}`);
+
+let textInput = []
+
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -37,6 +50,18 @@ app.get('/', function (req, res) {
 
 app.get('/test', function (req, res) {
     res.json(mockAPIResponse);
+})
+
+// async POST request
+app.post('/add', async function(req, res) {
+    textInput = req.body.url;
+    console.log(`you entered: ${textInput}`);
+    const apiURL = `${baseURL}key=${apiKey}&url=${textInput}&lang=en`
+
+    const response = await fetch(apiURL)
+    const data = await response.json()
+    console.log(data)
+    res.send(data)
 })
 
 // designates what port the app will listen to for incoming requests
