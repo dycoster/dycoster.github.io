e4040d3dc038c4c9506dd8141eff8e51423c5342
Your Name <you@example.com>
2021-01-14 11:31:21 +0100

    commit

--- Project04/evaluate-news-nlp/src/client/js/formHandler.js
+++ Project04/evaluate-news-nlp/src/client/js/formHandler.js
@@ -8,39 +8,31 @@ function handleSubmit(event) {
     const regex = new RegExp(/[A-Z.0-9]/gim);
     if(regex.test(formText)){
         document.getElementById('formResults').innerHTML = 'The results from the analyses:';
+
         console.log("::: Form Submitted :::");
 
     // post userInput to serverside
-    postData('http://localhost:8081/add', {text: formText})
+        fetch ('http://localhost:8081/add', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({text: formText}),
+        })
 
-    // receive response from API to updateUI
-    .then(function(res) {
-        document.getElementById('sentiment').innerHTML = `Cloudmeaning considers this text to be: ${res.subjectivity.toLowerCase()}`;
-        document.getElementById('confidence').innerHTML = `Cloudmeaning is ${res.confidence}% confident in this analyses.`;
-    });
+        .then (res => res.json())
+
+        // receive response from API to updateUI
+        .then(function(res) {
+            document.getElementById('subjectivity').innerHTML = `Cloudmeaning considers this text to be: ${res.subjectivity.toLowerCase()}`;
+            document.getElementById('confidence').innerHTML = `Cloudmeaning is ${res.confidence}% confident in this analyses.`;
+        });
     } else {
         document.getElementById('formResults').innerHTML = 'Invalid input, please try again';
     }
 }
 
-const postData = async (url = '', data = {})=> {
-    const response = await fetch (url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
-
-    try {
-        const newData = await response.json();
-        console.log(newData);
-        return newData;
-    }catch (error) {
-        console.log("error", error);
-    }
-}
 
 export { handleSubmit }
 
